9b3d07a3ab51aeb8dfa7e5824b95c3dd1fe5672b
ppj <1476848452@qq.com>
2022-07-07 14:30:14 +0800

    第一次提交，简单的demo

---
+++ src/main/java/com/kuang/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.kuang.git;
+
+/**
+ * @author ppj
+ * @date 2022/07/07
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+
+}
